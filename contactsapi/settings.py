8e8dce28180b835c1ed1bd546cd36ef50e362215
sravan-kumar-ta <sravan0095@gmail.com>
2022-10-27 16:03:53 +0530

    deploy on heroku

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -135,3 +136,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
